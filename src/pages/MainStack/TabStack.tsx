378e3f10b534de35fe164edc863a511e412dee7b
chikibrikie <legion777777777@mail.ru>
2024-01-30 18:56:07 +0300

    tab icon

--- src/pages/MainStack/TabStack.tsx
+++ src/pages/MainStack/TabStack.tsx
@@ -1,15 +1,33 @@
 import * as React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Icon } from "react-native-paper";
 
 import HomeBoard from "pages/Screens/ui/HomeBoard";
 import SCREENS from "shared/lib/screen";
+import SettingsScreen from "pages/Settings/ui/SettingsScreen";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabScreen() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === SCREENS.HomeBoard) {
+            iconName = focused ? "home-circle" : "home-circle-outline";
+          } else if (route.name === SCREENS.Settings) {
+            iconName = focused ? "cog" : "cog-outline";
+          }
+
+          // You can return any component that you like here!
+          return <Icon source={iconName} size={size} color={color} />;
+        },
+      })}
+    >
       <Tab.Screen name={SCREENS.HomeBoard} component={HomeBoard} />
+      <Tab.Screen name={SCREENS.Settings} component={SettingsScreen} />
     </Tab.Navigator>
   );
 }
